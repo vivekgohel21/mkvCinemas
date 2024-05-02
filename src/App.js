b773809d306debf890548edeed70cb9bbdb3e5d1
vivekgohel21 <vivekgohel001@gmail.com>
2024-05-02 15:14:33 +0530

    add changes

--- src/App.js
+++ src/App.js
@@ -107,6 +107,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState(0);
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
+  const watchedUserRating = watched.find((movie) => movie.imdbID === selectedId)?.userRating;
   let {
     Title: title,
     Year: year,
@@ -245,7 +246,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
                   }
                 </>
               ) : (
-                <p>You rated movie {userRating}/10.</p>
+                <p>You rated movie {watchedUserRating}/10.</p>
               )}
             </div>
             <p>
